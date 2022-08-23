3a72c72a4fce263d9d63bd3356350c768bbf57d0
szy <646932104@qq.com>
2022-08-23 14:17:30 +0800

    entropy bottleneck

--- aux_trainer_me.py
+++ aux_trainer_me.py
@@ -5,11 +5,12 @@ import random
 import numpy as np
 import torch.nn as nn
 import compressai
+
 from tqdm import tqdm
 from utils.util import *
-
+from utils.visualize import Visual
 from models.unet import UNet
-from models.spynet import SpyNet
+from models.spynet_tst import SpyNet
 from models.flow_ednet import MvanalysisNet, MvsynthesisNet
 
 from torch.autograd import Variable
@@ -22,16 +23,17 @@ if __name__ == "__main__":
     torch.backends.cudnn.enabled = True
     gpu_id = 5
     batch_size = 10
-    lr = 4e-4
+    lr = 1e-4
     epochs = 20
     device = torch.device('cuda:{}'.format(gpu_id))
     fixed = False
 
-    spynet_pretrain = f'./checkpoints/stage4.pth'
-    mv_encoder_pretrain = f'./checkpoints/motion_encoder_e.pth'
-    mv_decoder_pretrain = f'./checkpoints/motion_decoder_e.pth'
+    v = Visual('exp')
+    # spynet_pretrain = f'./checkpoints/stage4.pth'
+    mv_encoder_pretrain = f'./checkpoints/motion_encoder_d255.pth'
+    mv_decoder_pretrain = f'./checkpoints/motion_decoder_d255.pth'
 
-    spynet = get_model(SpyNet(), device, spynet_pretrain)
+    spynet = get_model(SpyNet(), device)
     mv_encoder = get_model(MvanalysisNet(device), device, mv_encoder_pretrain)
     mv_decoder = get_model(MvsynthesisNet(device), device, mv_decoder_pretrain)
 
@@ -46,36 +48,39 @@ if __name__ == "__main__":
     train_dataloader = DataLoader(dataset, batch_size=batch_size, sampler=sampler, pin_memory=True)
     mse_Loss = torch.nn.MSELoss(reduction='mean')
     entropy_loss = compressai.entropy_models.entropy_models.EntropyBottleneck(128).to(device)
+    global_step = 0
     try:
         for epoch in range(1, epochs):
-            with tqdm(total=n_train, desc=f'Epoch {epoch}/{epochs}') as pbar:
+            with tqdm(total=n_train, desc=f'Epoch {epoch}/{epochs}', ncols=110) as pbar:
                 for ref, cur in train_dataloader:
                     # [batch*frame, channel, width, height] -> # [batch, frame-1 , channel, width, height]
+                    height, width = ref.shape[2:]
                     ref = ref.to(device)
                     cur = cur.to(device)
-                    flows, _ = spynet(cur, ref)
+                    flows = spynet(cur, ref)
                     code, _ = mv_encoder(flows)
                     output, likelihood = entropy_loss(code)
                     res, _ = mv_decoder(output)
                     warped = optical_flow_warping(ref, res)
                     mse = mse_Loss(res, flows)
-                    loss = mse - (torch.log(likelihood)/math.log(2)).mean()
+                    psnr = -10.0 * torch.log10(1.0 / mse)
+                    loss = mse * 0.1 - torch.log(likelihood).sum() / height / width / batch_size / math.log(2)
                     optimizer_encoder.zero_grad()
                     optimizer_decoder.zero_grad()
                     loss.backward()
                     optimizer_encoder.step()
                     optimizer_decoder.step()
-                    pbar.set_postfix(**{'loss (batch)': format(loss.item(), '.5f')})
+                    pbar.set_postfix(**{'loss (batch)': format(loss.item(), '.2f'),
+                                        'mean res': format(res.max(), '.1f'),
+                                        'mean flo_res': format(flows.max(), '.1f')
+                                        })
                     pbar.update(ref.shape[0])
 
-                    save1 = cur[0].permute(1, 2, 0).cpu().detach().numpy()
-                    save2 = warped[0].permute(1, 2, 0).cpu().detach().numpy()
-                    save3 = ref[0].permute(1, 2, 0).cpu().detach().numpy()
-                    cv2.imwrite('./outs/res' + str(random.randint(1, 10)) + '.png',
-                                np.concatenate((save1, save2, save3), axis=1))
+                    v.draw_pics(global_step, [cur, warped, ref])
+                    global_step += 1
 
-        save_checkpoint(mv_encoder, 'motion_encoder_e')
-        save_checkpoint(mv_decoder, 'motion_decoder_e')
+        save_checkpoint(mv_encoder, 'motion_encoder_d2551')
+        save_checkpoint(mv_decoder, 'motion_decoder_d2551')
     except KeyboardInterrupt:
-        save_checkpoint(mv_encoder, 'motion_encoder_e')
-        save_checkpoint(mv_decoder, 'motion_decoder_e')
+        save_checkpoint(mv_encoder, 'motion_encoder_d2551')
+        save_checkpoint(mv_decoder, 'motion_decoder_d2551')
